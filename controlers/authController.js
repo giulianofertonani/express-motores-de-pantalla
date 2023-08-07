53ed837291ce7cf59e7989bf3b23cba6b9b5cfb6
giulianofertonani <giulianofertonani@gmail.com>
2023-08-06 21:42:00 -0300

    new file:   controlers/authController.js 	modified:   controlers/homeController.js
    	modified:   index.js
    	modified:   middlewares/urlValida.js
    	modified:   models/Url.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/js/app.js
    	modified:   routes/auth.js
    	modified:   routes/home.js
    	modified:   views/components/Form.hbs
    	modified:   views/components/Navbar.hbs
    	modified:   views/home.hbs
    modified:   views/layouts/main.hbs
    	modified:   views/login.hbs
    	.vscode/
    	middlewares/verificarUser.js
    	models/User.js
    	public/img/
    	views/register.hbs

---
+++ controlers/authController.js
@@ -0,0 +1,120 @@
+const User = require("../models/User");
+const{validationResult} = require("express-validator");
+const{nanoid} = require("nanoid");
+const nodemailer = require("nodemailer");
+require("dotenv").config()
+
+const registerForm = (req,res) =>{
+    return res.render("register")
+};
+
+const loginForm = (req,res) =>{
+    return res.render("login")
+};
+
+
+const registerUser = async (req,res) =>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        // return res.json(errors)
+        req.flash("mensajes", errors.array());
+        return res.redirect("/auth/register")
+    }
+
+    const {userName, email, password} = req.body;
+    try{
+        let user = await User.findOne({email: email});
+        if (user) throw new Error("ya existe el usuario");
+
+        user = new User({userName, email, password, tokenConfirm: nanoid()});
+        await user.save()
+
+        const transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: process.env.userEmail,
+              pass: process.env.passEmail
+            }
+          });
+
+          await transport.sendMail({
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: user.email, // list of receivers
+            subject: "Verifica tu cuenta de correo", // Subject line
+            html: `<a href="http://localhost:5000/auth/confirmar/${user.tokenConfirm}">Verifica tu cuenta aqui</a>`, // html body
+          });
+
+        req.flash("mensajes",[
+            {msg: "Revisa tu correo electronico y valida tu cuenta",
+        }]);
+        return res.redirect("/auth/login")
+    }catch(error){
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/auth/register")
+    }
+};
+
+const confirmarCuenta = async(req,res) =>{
+    const{ token } = req.params
+    try{
+        const user = await User.findOne({ tokenConfirm: token});
+        if(!user) throw new Error("no existe este usuario");
+
+        user.cuentaConfirmada = true;
+        user.tokenConfirm = null;
+
+        await user.save();
+
+        req.flash("mensajes",[{msg: "Cuenta verificada, puedes iniciar sesion"}]);
+        return res.redirect("/auth/login");
+    }catch(error){
+        // return res.json({error: error.message})
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/auth/login")
+    }
+};
+
+const loginUser =async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        req.flash("mensajes", errors.array());
+        return res.redirect("/auth/login")
+    }
+    
+    const {email,password} =req.body;
+    try{
+        const user = await User.findOne({email});
+        if (!user) throw new Error("No existe este email");
+
+        if (!user.cuentaConfirmada)throw new Error("Falta confirmar cuenta");
+
+        if (!await user.comparePassword(password))throw new Error("Contraseña invalida");
+
+        // creando la sesion de usuario a traves de passport
+        req.login(user, function (err){
+            if(err) throw new Error("Error al crear la sesion");
+        })
+        return res.redirect("/"); 
+    }catch(error){
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/auth/login")
+        // console.log({error})
+        // return res.send(error.message)
+    }
+};
+
+const cerrarSesion = (req,res) =>{
+    req.logout();
+    return res.redirect ( "/auth/login");
+}
+
+
+module.exports = {
+    loginForm,
+    registerForm,
+    registerUser,
+    confirmarCuenta,
+    loginUser,
+    cerrarSesion,
+}
--- controlers/homeController.js
+++ controlers/homeController.js
@@ -2,70 +2,102 @@ const Url = require("../models/Url")
 const { nanoid } = require("nanoid");
 
 const leerUrls = async(req,res) => {
+    // console.log(req.user)
     try{
-        const urls = await Url.find().lean()
-        res.render("home",{urls:urls});
+        const urls = await Url.find({ user: req.user.id}).lean();
+        return res.render("home",{ urls: urls });
     }catch(error){
-        console.log(error)
+        // console.log(error)
+        // return res.send("fallo algo...")
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/")
     }
 };
 
 const agregarUrl= async (req,res) =>{
     const { origin} = req.body;
-
+    // console.log(req.user)
 
     try{
-        const url = new Url({origin: origin,shortURL: nanoid(8)})
+        const url = new Url({
+            origin: origin,
+            shortURL: nanoid(8), 
+            user: req.user.id,
+        });
         await url.save();
+        req.flash("mensajes",[{msg: "Url agregada"}]);
         res.redirect("/");
     }catch(error){
-        console.log(error);
-        res.send("error algo fallo")
+        console.log(error)
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/")
     }
 };
 
 const eliminarUrl = async(req,res) =>{
-    const {id} =req.params
+    const { id } =req.params;
+    //console.log(req.user)
     try{
-        await Url.findByIdAndDelete(id);
-        res.redirect("/");
+        // await Url.findByIdAndDelete(id);
+        const url = await Url.findById(id);
+        if(!url.user.equals(req.user.id)) {
+            throw new Error("No es tu url")
+        }
+
+        await url.deleteOne()
+        req.flash("mensajes",[{msg: "Url eliminada"}]);
+        return res.redirect("/");
 
     }catch(error){
-        console.log(error);
-        res.send("error algo fallo")
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/")
     }
 }
 
 const editarUrlForm = async(req,res) =>{
     const {id} =req.params
     try{
-        
         const url = await Url.findById(id).lean();
-        res.render("home", { url });
+
+        if(!url.user.equals(req.user.id)) {
+            throw new Error("No es tu url")
+        }
+
+        return res.render("home", { url });
     }catch(error){
-        console.log(error);
-        res.send("error algo fallo")
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/")
     }
 }
 const editarUrl= async(req,res) =>{
     const {id} =req.params;
     const {origin} = req.body;
     try{
-        await Url.findByIdAndUpdate(id, {origin: origin})
+        const url = await Url.findById(id);
+        if(!url.user.equals(req.user.id)) {
+            throw new Error("No es tu url")
+        }
+
+        await url.updateOne({origin});
+        req.flash("mensajes",[{msg: "Url editada"}]);
+
+        // await Url.findByIdAndUpdate(id, {origin: origin})
         res.redirect("/");
     }catch(error){
-        console.log(error);
-        res.send("error algo fallo")
+        req.flash("mensajes",[{msg: error.message}]);
+        return res.redirect ( "/")
     }
 }
 
-const redireccionamiento =async(req,res) =>{
+const redireccionamiento = async(req,res) =>{
     const {shortURL} = req.params;
     console.log(shortURL)
     try{
         const urlDB = await Url.findOne({ shortURL : shortURL })
         res.redirect(urlDB.origin)
     }catch(error){
+        req.flash("mensajes",[{msg: "No existe esta url configurada"}]);
+        return res.redirect ( "/auth/login")
     }
 }
 
